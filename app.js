f340d310bed5354a1be931ea7418b57e9b00465b
Cameron Blanchard <blanchardc349@gmail.com>
2021-05-15 17:29:32 -0600

    functional not done

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+var totalCheese = 0
+
 let clickUpgrades = {
   pick: {
     price: 10,
@@ -24,7 +26,6 @@ let automaticUpgrades = {
   }
 }
 
-var totalCheese = 0
 
 
 function mine() {
@@ -35,9 +36,85 @@ function mine() {
 }
 
 function update() {
-  let template = totalCheese
-  let totalCheeseElem = document.getElementById("cheese-count")
-  totalCheeseElem.innerHTML = template.toString()
+  disableButtons()
+  let template = `<div id="resources">
+  <div class="row">
+    <div class="col-12">
+      <span class="" >Cheese: <span id="cheese-count">${totalCheese}</span></span>
+      <div class="row">
+        <div class="col">
+          <span class="">Pickaxes: <span id="pick-quantity">${clickUpgrades.pick.quantity}     Multiplier: ${clickUpgrades.pick.multiplier}</span></span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <span>Carts: <span id="cart-quantity">${clickUpgrades.cart.quantity}</span></span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <span>Miners: <span id="miner-quantity">${automaticUpgrades.miner.quantity}</span></span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <span>Rovers: <span id="rover-quantity">${automaticUpgrades.rover.quantity}</span></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+`
+let resourcesElem = document.getElementById("resources")
+resourcesElem.innerHTML = template
+
+let template2 = `
+<div id="store">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="row">
+                <div class="col-6">
+                  <span id="pick-price" class="d-flex justify-content-start m-1">Pickaxe Price: ${clickUpgrades.pick.price}</span>
+                </div>
+                <div class="col-6">
+                  <span class="d-flex justify-content-end p-1"><button id="pick-btn" onclick="buyPick()"
+                      class=" btn-success m-1">BUY</button></span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6">
+                  <span id="cart-price" class="d-flex justify-content-start m-1">Cart Price: ${clickUpgrades.cart.price}</span>
+                </div>
+                <div class="col-6">
+                  <span class="d-flex justify-content-end p-1"><button id="cart-btn" onclick="buyCart()"
+                      class=" btn-success m-1">BUY</button></span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6">
+                  <span id="miner-price" class="d-flex justify-content-start m-1">Miner Price ${automaticUpgrades.miner.price}:</span>
+                </div>
+                <div class="col-6">
+                  <span class="d-flex justify-content-end p-1"><button id="miner-btn" onclick="buyMiner()"
+                      class=" btn-success m-1">BUY</button></span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-6">
+                  <span id="rover-price" class="d-flex justify-content-start m-1">Rover Price: ${automaticUpgrades.rover.price}</span>
+                </div>
+                <div class="col-6">
+                  <span class="d-flex justify-content-end p-1"><button id="rover-btn" onclick="buyRover()"
+                      class=" btn-success m-1">BUY</button></span>
+              </div>
+            </div>
+          </div>
+        
+`
+let storeElem = document.getElementById("store")
+storeElem.innerHTML = template2
+
 }
 function buyPick() {
   if (totalCheese >= clickUpgrades.pick.price) {
@@ -48,6 +125,7 @@ function buyPick() {
     console.log("new pick quantity: ", clickUpgrades.pick.quantity)
     console.log("new pick price: ", clickUpgrades.pick.price)
   }
+  update()
 }
 function buyCart() {
   if (totalCheese >= clickUpgrades.cart.price) {
@@ -59,24 +137,25 @@ function buyCart() {
     console.log("new cart quantity: ", clickUpgrades.cart.quantity)
     console.log("new cart price: ", clickUpgrades.cart.price)
   }
+  update()
 }
 
-function buyMiner(){
+function buyMiner() {
   if (totalCheese >= automaticUpgrades.miner.price) {
     automaticUpgrades.miner.quantity += 1
     totalCheese -= automaticUpgrades.miner.price
     automaticUpgrades.miner.price *= 3
-    
+
     console.log("cheese: ", totalCheese)
     console.log("new miner quantity: ", automaticUpgrades.miner.quantity)
     console.log("new miner price: ", automaticUpgrades.miner.price)
     startInterval()
     collectAutoUpgrades()
-    update()
   }
+  update()
 }
 
-function buyRover(){
+function buyRover() {
   if (totalCheese >= automaticUpgrades.rover.price) {
     automaticUpgrades.rover.quantity += 1
     totalCheese -= automaticUpgrades.rover.price
@@ -87,26 +166,38 @@ function buyRover(){
     console.log("new rover price: ", automaticUpgrades.rover.price)
     startInterval()
     collectAutoUpgrades()
-  }
+ }
+ update()
 }
-function collectClickUpgrades(){
+function collectClickUpgrades() {
   let clickModifier = (clickUpgrades.pick.quantity * clickUpgrades.pick.multiplier) + (clickUpgrades.cart.quantity * clickUpgrades.cart.multiplier)
   totalCheese += clickModifier
-  
+  update()
+
 }
 
 
 
-function collectAutoUpgrades(){
-  for(let key in automaticUpgrades){
+function collectAutoUpgrades() {
+  for (let key in automaticUpgrades) {
     let autoModifier = automaticUpgrades[key].quantity * automaticUpgrades[key].multiplier
     totalCheese += autoModifier
+    update()
   }
   console.log(totalCheese)
 }
 
-function startInterval(){
+function startInterval() {
   let collectionInterval = setInterval(collectAutoUpgrades, 3000)
 }
-//collectAutoUpgrades()
-//startInterval()
+
+function disableButtons() {
+  totalCheese < clickUpgrades.pick.price ? document.getElementById("pick-btn").classList.add("disabled") : document.getElementById("pick-btn").classList.remove("disabled")
+
+  totalCheese < clickUpgrades.cart.price ? document.getElementById("cart-btn").classList.add("disabled") : document.getElementById("cart-btn").classList.remove("disabled")
+
+  totalCheese < automaticUpgrades.miner.price ? document.getElementById("miner-btn").classList.add("disabled") : document.getElementById("miner-btn").classList.remove("disabled")
+
+  totalCheese < automaticUpgrades.rover.price ? document.getElementById("rover-btn").classList.add("disabled") : document.getElementById("rover-btn").classList.remove("disabled")
+}
+update()
